75c3cda7f4b2a7de3ad47a7fefb610d377f955f0
Hamzafai <hamza.faiz.ahmedfouatih@ensia.edu.dz>
2025-10-22 10:29:07 +0100

    Push from past semester (2nd year)

---
+++ Utils.py
@@ -0,0 +1,199 @@
+def first(iterable, default= None):
+    from itertools import chain, combination
+    return next(iter(iterable), default)
+
+def argmin_random_tie(seq, key = lambda x: x):
+    return min(shuffled(seq), key = key)
+
+def shuffled(iterbale):
+    import random
+    items = list(iterbale)
+    random.shuffle(items)
+    return items
+
+
+class Expr:
+    """ a mathetical expression with an operator and 0 or more args """
+    def __init__(self, op, *args):
+        self.op = op
+        self.args = args
+
+    def __neg__(self):
+        return Expr('-', self)
+
+    def __pos__(self):
+        return Expr('+', self)
+
+    def __invert__(self):
+        return Expr('~', self)
+
+    def __add__(self, rhs):
+        return Expr('+', self, rhs)
+
+    def __sub__(self, rhs):
+        return Expr('-', self, rhs)
+
+    def __mul__(self, rhs):
+        return Expr('*', self, rhs)
+
+    def __pow__(self, rhs):
+        return Expr('**', self, rhs)
+
+    def __mod__(self, rhs):
+        return Expr('%', self, rhs)
+
+    def __and__(self, rhs):
+        return Expr('&', self, rhs)
+
+    def __xor__(self, rhs):
+        return Expr('^', self, rhs)
+
+    def __rshift__(self,rhs):
+        return Expr('>>', self, rhs)
+
+    def __lshift__(self, rhs):
+        return Expr('<<', self, rhs)
+
+    def __floordiv__(self, rhs):
+        return Expr('//', self, rhs)
+
+    def __matmul__(self, rhs):
+        return Expr('@', self, rhs)
+
+    def __truediv__(self, rhs):
+        return Expr('/', self, rhs)
+
+    def __or__(self, rhs):
+        """Allow both P | Q, and P |'==>'| Q."""
+        if isinstance(rhs, Expression):
+            return Expr('|', self, rhs)
+        else:
+            return PartialExpr(rhs, self)
+
+    # Reverse operator overloads
+    def __radd__(self, lhs):
+        return Expr('+', lhs, self)
+
+    def __rsub__(self, lhs):
+        return Expr('-', lhs, self)
+
+    def __rmul__(self, lhs):
+        return Expr('*', lhs, self)
+
+    def __rdiv__(self, lhs):
+        return Expr('/', lhs, self)
+
+    def __rpow__(self, lhs):
+        return Expr('**', lhs, self)
+
+    def __rmod__(self, lhs):
+        return Expr('%', lhs, self)
+
+    def __rand__(self, lhs):
+        return Expr('&', lhs, self)
+
+    def __rxor__(self, lhs):
+        return Expr('^', lhs, self)
+
+    def __ror__(self, lhs):
+        return Expr('|', lhs, self)
+
+    def __rrshift__(self, lhs):
+        return Expr('>>', lhs, self)
+
+    def __rlshift__(self, lhs):
+        return Expr('<<', lhs, self)
+
+    def __rtruediv__(self, lhs):
+        return Expr('/', lhs, self)
+
+    def __rfloordiv__(self, lhs):
+        return Expr('//', lhs, self)
+
+    def __rmatmul__(self, lhs):
+        return Expr('@', lhs, self)
+
+    def __call__(self, *args):
+        """ if f is a symbol , then f(0) == Expr('f',0) . """
+        if self.args:
+            raise ValueError('can only do a call for a sumbol not Expr')
+        else:
+            return Expr(self.op, *args)
+
+    def __eq__(self, other):
+        return isinstance(other, Expr) and self.op == other.op and self.args == other.args
+
+    def __lt__(self, other):
+        return isinstance(other, Expr) and str(self) < str(other)
+
+    def __hash__(self):
+        return hash(self.op) ^ hash(self.args)
+
+    def __repr__(self):
+        """ It defines how expressions are printed in a readable format. """
+        op = self.op
+        args = [str(arg) for arg in self.args]
+        if op.isidentifier(): # f(x) or f(x, y)
+            return '{}({})'.format(op, ', '.join(args)) if args else op
+        elif len(args) == 1: # *x or -(x+1)
+            return op +args[0]
+        else: # ( x-y )
+            opp = (' '+ op +' ')
+            return '(' + opp.join(args) + ')'
+
+# An 'Expression' is either an Expr or a Number.
+# Symbol is not an explicit type; it is any Expr with 0 args.
+
+
+Number = (int, float, complex)
+Expression = (Expr, Number)
+
+class PartialExpr:
+    """Given 'P |'==>'| Q, first form PartialExpr('==>', P), then combine with Q."""
+
+    def __init__(self, op, lhs):
+        self.op, self.lhs = op, lhs
+
+    def __or__(self, rhs):
+        return Expr(self.op, self.lhs, rhs)
+
+    def __repr__(self):
+        return "PartialExpr('{}', {})".format(self.op, self.lhs)
+
+def Symbol(name):
+    return Expr(name)
+
+from collections import defaultdict
+class defaultkeydict(defaultdict):
+    def __missing__(self, key):
+        self[key] = result = self.default_facotry(key)
+        return result
+        
+def expr(x):
+    """Shortcut to create an Expression. x is a str in which:
+    - identifiers are automatically defined as Symbols.
+    - ==> is treated as an infix |'==>'|, as are <== and <=>.
+    If x is already an Expression, it is returned unchanged. Example:
+    >>> expr('P & Q ==> Q')
+    ((P & Q) ==> Q)
+    """
+    return eval(expr_handle_infix_ops(x), defaultkeydict(Symbol)) if isinstance(x, str) else x
+
+
+infix_ops = '==> <== <=>'.split()
+
+
+def expr_handle_infix_ops(x):
+    """Given a str, return a new str with ==> replaced by |'==>'|, etc.
+    >>> expr_handle_infix_ops('P ==> Q')
+    "P |'==>'| Q"
+    """
+    for op in infix_ops:
+        x = x.replace(op, '|' + repr(op) + '|')
+    return x
+
+def subexpressions(x):
+    yield x
+    if isinstance(x, Expr):
+        for arg in x.args:
+            yield from subexpressions(arg)
